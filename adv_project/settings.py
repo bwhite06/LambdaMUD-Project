b8988e8aa6bd5993216d928fe3f142a5aa4a77d5
Brandon White <bwhite06@stac.edu>
2018-12-12 14:55:55 -0500

    updated settings file

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 
