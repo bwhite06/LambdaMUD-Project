0fc61ae7f9027eafa1e813ed02bda56f92e867e8
Brandon White <bwhite06@stac.edu>
2018-12-12 14:26:09 -0500

    updated settings

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -154,7 +154,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 
 import dj_database_url
